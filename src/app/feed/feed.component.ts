d9597dd45bebfb77d8a82d1540fda55d2ed9e2da
Mariel <marielk.mya@gmail.com>
2018-10-09 16:14:37 -0300

    arreglado el scroll

--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -9,17 +9,22 @@ import { ImageServiceService } from '../image-service.service';
 
 
 export class FeedComponent implements OnInit {
-  @HostListener('window:scroll', ['$event'])
+  @HostListener('scroll', ['$event'])
 
-  visibleImages = [];
-  visibleImages2 = [];
+  visibleImages: any[] = [];
+  visibleImages2: any[] = [];
   constructor(private imageService: ImageServiceService) {
     this.visibleImages = this.imageService.getImagesFetch();
     // console.log(this.visibleImages);
   }
 
   onScrollUp($event: Event) {
-    this.visibleImages2 = this.imageService.getFectchAgain();
+    const div = document.getElementById('main');
+    console.log(div.scrollTop);
+    const scroll = div.scrollTop;
+    if (scroll === 2815) {
+      this.visibleImages2 = this.imageService.getFectchAgain();
+    }
   }
 
   ngOnInit() {
--- src/app/image-service.service.ts
+++ src/app/image-service.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http' ;
+import { Observable } from 'rxjs';
 
 
 @Injectable({
@@ -7,10 +8,10 @@ import { HttpClient } from '@angular/common/http' ;
 })
 
 export class ImageServiceService {
-  visibleImages = [];
-  visibleImages2 = [];
-  IMAGES = [];
-  IMAGES2 = [];
+  visibleImages: any[] = [];
+  visibleImages2: any[] = [];
+  IMAGES: any[] = [];
+  IMAGES2: any[] = [];
   accesKey = 'client_id=f096278b5be3b39a1319858a4841c39c2fbc1e4a9be7dad87079617db9d9ec69';
   endPoint = 'https://api.unsplash.com/photos/?';
   params = '&per_page=20';
@@ -45,4 +46,6 @@ export class ImageServiceService {
       );
     });
     return this.visibleImages2 = this.IMAGES2;
-  }}
+  }
+
+}
